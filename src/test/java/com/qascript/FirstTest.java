4c5c888db809ae8841d5e7481b99fd1d99b3602c
x7k <58208270+x7k@users.noreply.github.com>
2022-09-07 20:47:57 +0200

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FirstTest {
@@ -13,14 +12,14 @@ public class FirstTest {
     @Test
     public void OpenBrowser()  {
         
-        WebDriverManager.firefoxdriver().setup();
-        WebDriver driver;
+        WebDriverManager.chromedriver().setup();
         
-        System.setProperty("webdriver.chrome.driver", "/snap/bin/chromium.chromedriver");
+        
+        
         ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
+        WebDriver driver = new ChromeDriver(options);
         
-        driver = new FirefoxDriver(options);
         driver.get("https://www.google.com");
         System.out.println("Title of the page is: " + driver.getTitle());
         Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
@@ -28,5 +27,4 @@ public class FirstTest {
 
 
 }
-
 
