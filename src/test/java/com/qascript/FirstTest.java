6999b8176eef538bc250ff35bd291c4e47ae5872
x7k <58208270+x7k@users.noreply.github.com>
2022-09-07 20:50:23 +0200

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -5,19 +5,21 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager;
+
+//import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FirstTest {
 
     @Test
     public void OpenBrowser()  {
         
-        WebDriverManager.chromedriver().setup();
+        // WebDriverManager.chromedriver().setup();
         
-        
+        System.setProperty("webdriver.chrome.driver", "/snap/bin/chromium.chromedriver");
         
         ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
+        options.addArguments("disable-gpu");
         WebDriver driver = new ChromeDriver(options);
         
         driver.get("https://www.google.com");
